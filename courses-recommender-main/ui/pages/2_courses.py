a8357512a8c2684a80eb17687b822b08640b1057
amine <m.moussi@esi-sba.dz>
2025-01-31 00:22:26 +0100

    Uploaded local files and added new changes

---
+++ courses-recommender-main/ui/pages/2_courses.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os
+
+st.set_page_config(
+  page_title='courses recommender',
+  page_icon="📚",
+  layout="centered",
+  # initial_sidebar_state: InitialSideBarState = "auto"
+)
+
+# # courses
+data_folder_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
+
+all_courses = pd.read_csv('ui/data/all_courses.csv')
+rated_courses = pd.read_csv('ui/data/rated_courses.csv')
+ratings_courses = pd.read_csv('ui/data/ratings_courses.csv')
+
+st.header('📈 data story:')
+
+st.warning('within our datasets, we have the set of all courses, from it we extracted only the rated ones which are the ones we are interested in.')
+
+# all courses
+st.subheader('∀ all courses:')
+
+st.markdown('##### all courses head: ')
+st.dataframe(all_courses.head())
+st.markdown('##### all courses tail: ')
+st.dataframe(all_courses.tail())
+
+col1, col2, col3 = st.columns(3)
+
+all_courses_number = all_courses['course_id'].nunique()
+all_courses_missing_vals = all_courses.isna().sum().sum()
+all_courses_duplicated_vals = all_courses.duplicated().sum()
+
+col1.metric("number of courses", f'{all_courses_number} 📚')
+col2.metric("missing values", f'{all_courses_missing_vals} ❔')
+col3.metric("number of duplicates", f'{all_courses_duplicated_vals} 🗐')
+
+# rated courses  
+st.subheader('∃ only rated courses:')
+
+st.markdown('##### rated courses head: ')
+st.dataframe(all_courses.head())
+st.markdown('##### rated courses tail: ')
+st.dataframe(all_courses.tail())
+
+col1, col2, col3 = st.columns(3)
+
+rated_courses_number = rated_courses['course_id'].nunique()
+rated_courses_missing_vals = rated_courses.isna().sum().sum()
+rated_courses_duplicated_vals = rated_courses.duplicated().sum()
+
+col1.metric("number of courses", f'{rated_courses_number} 📚')
+col2.metric("missing values", f'{rated_courses_missing_vals} ❔')
+col3.metric("number of duplicates", f'{rated_courses_duplicated_vals} 🗐')
+
+courses_ratings_summery = ratings_courses.groupby(by='course_id')['rating'].agg(['mean', 'count']).reset_index().rename(columns={'mean': 'average_rating', 'count': 'ratings_count'}).merge(rated_courses)
+
+st.subheader('rated courses summery: ')
+st.dataframe(courses_ratings_summery.head())
+
+best_rated_courses = courses_ratings_summery.sort_values(by='average_rating', ascending=False).head(10)
+worst_rated_courses = courses_ratings_summery.sort_values(by='average_rating').head(10)
+most_rated_courses = courses_ratings_summery.sort_values(by='ratings_count',  ascending=False).head(10)
+
+fig = px.bar(best_rated_courses, x='title', y='average_rating', orientation='v', title='Best Rated Courses')
+st.plotly_chart(fig)
+
+fig = px.bar(worst_rated_courses, x='title', y='average_rating', orientation='v', title='Worst Rated Courses')
+st.plotly_chart(fig)
+
+fig = px.bar(most_rated_courses, x='title', y='ratings_count', orientation='v', title='Most Rated Courses')
+st.plotly_chart(fig)
---
+++ courses-recommender-main/ui/pages/3_courses_suggestion.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import pickle
+from models.fp_growth import fp_growth_recommend
+from models.apriori import apriori_recommend
+from mlxtend.frequent_patterns import fpgrowth, association_rules
+
+st.set_page_config(
+    page_title='Courses Recommender',
+    page_icon="📚",
+    layout="centered",
+)
+
+# Use caching to load data and rules
+@st.cache_data
+def load_data():
+    clean_user_with_courses = pd.read_csv('ui/data/clean_user_with_courses.csv')
+    rated_courses = pd.read_csv('ui/data/rated_courses.csv')
+    fp_growth_rules = pd.read_csv('ui/data/fp_growth_association_rules.csv')
+    apriori_rules = pd.read_csv('ui/data/apriori_association_rules.csv')
+    return clean_user_with_courses, rated_courses, fp_growth_rules, apriori_rules
+
+clean_user_with_courses, rated_courses, fp_growth_rules, apriori_rules = load_data()
+
+
+# UI
+st.title('Explore New Courses 📚')
+
+selected_titles = st.multiselect(
+    "# Select the courses you've watched",
+    rated_courses['title'], 
+    help="Select the courses you have completed to get tailored recommendations.",
+)
+
+selected_ids = rated_courses[rated_courses['title'].isin(selected_titles)]['course_id'].to_list()
+
+algorithm = st.selectbox(
+    "Choose an algorithm for recommendations:",
+    ["apriori", "eclat", "fp_growth"],
+    help="Choose the algorithm you prefer for generating course recommendations."
+)
+
+if st.button("Get Recommendations"):
+    if algorithm == 'apriori':
+        recommendations = apriori_recommend(apriori_rules, selected_ids)
+    elif algorithm == 'eclat':
+        pass
+    else:
+        recommendations = fp_growth_recommend(fp_growth_rules, selected_ids)
+    if recommendations:
+        recommended_courses = rated_courses[rated_courses['course_id'].isin(recommendations)]['title'].tolist()
+        st.write("Recommended courses:", recommended_courses)
+    else:
+        st.write("No recommendations found.")
